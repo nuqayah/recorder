0cd6cedf599339a8074cd23260b31579c9f1be2f
Slmaan Albeady <salbeady@gmail.com>
2025-02-28 14:50:15 +0300

    shadcn: add basic components

--- src/Home.svelte
+++ src/Home.svelte
@@ -1,1 +1,9 @@
-<h1 class="text-center text-4xl font-bold">Svelte App</h1>
+<div class="mt-8 flex flex-col items-center gap-4">
+    <h1 class="text-center text-4xl font-bold">Svelte App</h1>
+
+    <Button variant="outline" class="text-red-500">!shadcn-svelte</Button>
+</div>
+
+<script>
+import {Button} from '$lib/components/ui/button/index.js'
+</script>
