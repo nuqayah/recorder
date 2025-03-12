027be5d0dacae4e4928cc26537f01d1576e20b74
Slmaan Albeady <salbeady@gmail.com>
2025-03-12 12:03:07 +0300

    shadcn: add card, input, taps & label

---
+++ src/lib/components/ui/card/card-title.svelte
@@ -0,0 +1,15 @@
+<div
+    role="heading"
+    aria-level={level}
+    bind:this={ref}
+    class={cn('text-2xl leading-none font-semibold tracking-tight', className)}
+    {...restProps}
+>
+    {@render children?.()}
+</div>
+
+<script>
+import {cn} from '$lib/utils.js'
+
+let {ref = $bindable(null), class: className, level = 3, children, ...restProps} = $props()
+</script>
---
+++ src/lib/components/ui/card/card.svelte
@@ -0,0 +1,13 @@
+<div
+    bind:this={ref}
+    class={cn('bg-card text-card-foreground rounded-lg border shadow-sm', className)}
+    {...restProps}
+>
+    {@render children?.()}
+</div>
+
+<script>
+import {cn} from '$lib/utils.js'
+
+let {ref = $bindable(null), class: className, children, ...restProps} = $props()
+</script>
